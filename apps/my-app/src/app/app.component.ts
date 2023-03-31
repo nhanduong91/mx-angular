acf464ad0513c4beab3f82f32c616239d763bacf
Nhan Duong <nhan.duonghoang91@gmail.com>
2023-03-31 17:15:11 +0700

    create branch

--- apps/my-app/src/app/app.component.ts
+++ apps/my-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'stackblitz-nx-angular-root',
+  selector: 'nx-angular-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
